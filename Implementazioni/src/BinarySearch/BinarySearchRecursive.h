3fb56c95a2b0cf47273d7f7d19bc068128ea95df
Richard Paris <Antonino117@hotmail.it>
2022-12-26 13:22:05 +0100

    Update

--- Implementazioni/src/BinarySearch/BinarySearchRecursive.h
+++ Implementazioni/src/BinarySearch/BinarySearchRecursive.h
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 int binarySearch(int* array, size_t size, int element) {
-    printf("[debug]: called funct array[size/2] = %d, size = %lu\n, subarray[%d-%d]\n", array[size/2], size, array[0], array[size - 1]);
     if(size == 0) {
         return 1;
     }
-    if(array[size/2] == element) {
-        printf("[debug]: found %d\n", array[size/2]);
+    size = size/2;
+    if(array[size - 1] == element) {
+        printf("[debug]: found %d\n", array[size - 1]);
         return 0;
     } 
 
-    if(array[size/2] > element) {
-        return binarySearch(array, (size/2) - 1, element);
+    if(array[size - 1] > element) {
+        return binarySearch(array, size - 1, element);
     } else {
-        return binarySearch(array + (size/2), (size/2) + 1, element);
+        return binarySearch(array + size, size + 1, element);
     }
 }
